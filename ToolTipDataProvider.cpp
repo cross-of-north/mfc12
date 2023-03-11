fad81bc477fad87a52aa67c69b9c012685efda04
cross.of.north <cross.of.north@gmail.com>
2023-03-12 03:26:07 +0700

    Comments.

--- ToolTipDataProvider.cpp
+++ ToolTipDataProvider.cpp
@@ -2,18 +2,7 @@
 #include "pch.h"
 #include "ToolTipDataProvider.h"
 
-
-class CCustomMFCRibbonButton: public CMFCRibbonButton {
-public:
-    CCustomMFCRibbonButton( UINT nID, LPCTSTR lpszText, HICON hIcon, BOOL bAlwaysShowDescription = FALSE, HICON hIconSmall = NULL, BOOL bAutoDestroyIcon = FALSE, BOOL bAlphaBlendIcon = FALSE )
-        : CMFCRibbonButton( nID, lpszText, hIcon, bAlwaysShowDescription, hIconSmall, bAutoDestroyIcon, bAlphaBlendIcon ) {
-        m_bIsLargeImage = TRUE;
-    }
-    virtual ~CCustomMFCRibbonButton() {
-    }
-};
-
-// the hack to access internal structures from here
+// hacks to access internal MFC structures from here
 class friendly_CMFCToolTipCtrl: public CMFCToolTipCtrl {
     friend class CToolTipDataProvider;
 };
@@ -24,6 +13,17 @@ class friendly_CMFCPropertySheet: public CMFCPropertySheet {
     friend class CMFCPropertySheetToolTipDataProvider;
 };
 
+// This class created only to set m_bIsLargeImage = TRUE.
+// Since the instances of CMFCRibbonButton are created here, it is possible to do without "friendly" hacks.
+class CCustomMFCRibbonButton: public CMFCRibbonButton {
+public:
+    CCustomMFCRibbonButton( UINT nID, LPCTSTR lpszText, HICON hIcon, BOOL bAlwaysShowDescription = FALSE, HICON hIconSmall = NULL, BOOL bAutoDestroyIcon = FALSE, BOOL bAlphaBlendIcon = FALSE )
+        : CMFCRibbonButton( nID, lpszText, hIcon, bAlwaysShowDescription, hIconSmall, bAutoDestroyIcon, bAlphaBlendIcon ) {
+        m_bIsLargeImage = TRUE;
+    }
+    virtual ~CCustomMFCRibbonButton() {
+    }
+};
 
 
 // CToolTipDataProviderProperties
@@ -44,13 +44,15 @@ CToolTipDataProviderProperties::~CToolTipDataProviderProperties() {
 // CToolTipDataProvider
 
 CToolTipDataProvider::CToolTipDataProvider( CToolTipDataProviderList * list )
-: m_list( list )
+    : m_list( list )
 {
     Subscribe( m_list );
 }
 
 CToolTipDataProvider::~CToolTipDataProvider() {
     Unsubscribe();
+
+    // delete tooltip props structures
     POSITION pos = m_toolTipDataMap.GetStartPosition();
     while ( pos != NULL ) {
         UINT key = 0;
@@ -75,40 +77,65 @@ void CToolTipDataProvider::Unsubscribe() {
 
 BOOL CToolTipDataProvider::OnGetToolTipText( CMFCToolBarButton * pButton, CString & strTTText ) {
 
+    // This is the first chance to perform tooltip-related activity.
+    // We are creating tooltip properies storage and call FillToolTipProperties to get customized data.
     CToolTipDataProviderProperties * props = new CToolTipDataProviderProperties( pButton );
     BOOL bResult = FillToolTipProperties( *props );
 
     if ( bResult ) {
+
+        // If properties data is customized, we save it for later usage.
+
         // Not all buttons have command IDs set, but we need ID to differentiate buttons in OnGetMessageString().
         // When there is no command ID, we set low DWORD of button address (sufficiently unique) as a command ID.
         if ( pButton->m_nID == 0 ) {
             pButton->m_nID = PtrToUint( pButton );
         }
+
+        // Overwriting old props value.
         CToolTipDataProviderProperties * old_props = NULL;
         m_toolTipDataMap.Lookup( pButton->m_nID, old_props );
         if ( old_props != NULL ) {
             delete old_props;
         }
         m_toolTipDataMap[ pButton->m_nID ] = props;
+
+        // Setting toolbar text from props.
         strTTText = props->m_strText;
-        if ( props->m_hIcon != NULL && props->m_pToolTip != NULL ) {
-            delete props->m_pRibbonButton;
-            props->m_pRibbonButton = new CCustomMFCRibbonButton( 0, L"", props->m_hIcon );
+
+        if ( props->m_pToolTip != NULL ) {
+            
+            // Updating icon.
             friendly_CMFCToolTipCtrl * pToolTip = static_cast < friendly_CMFCToolTipCtrl * > ( props->m_pToolTip );
-            pToolTip->m_pRibbonButton = props->m_pRibbonButton;
+            
+            if ( props->m_hIcon != NULL ) {
+                // Creating CMFCRibbonButton instance is the only way to provide the tooltip icon here.
+                props->m_pRibbonButton = new CCustomMFCRibbonButton( 0, L"", props->m_hIcon );
+                pToolTip->m_pRibbonButton = props->m_pRibbonButton;
+            }
+
+            // Updating other parameters.
             props->m_pToolTip->SetParams( &props->m_props );
         }
+
     } else {
+
+        // No need to store data if the tooltip is not customized.
         delete props;
+
     }
 
     return bResult;
 }
 
 BOOL CToolTipDataProvider::OnGetMessageString( UINT nID, CString & rMessage ) {
+    
+    // Looking up tooltip properies by command ID
     CToolTipDataProviderProperties * props = NULL;
     m_toolTipDataMap.Lookup( nID, props );
+
     if ( props != NULL && !props->m_strDescription.IsEmpty() ) {
+        // If button is found, using description saved while processing OnGetToolTipText (which always comes before OnGetMessageString).
         rMessage = props->m_strDescription;
         return TRUE;
     }
@@ -203,10 +230,14 @@ BOOL CMFCPropertySheetToolTipDataProvider::FillToolTipProperties( CToolTipDataPr
     CMFCPropertyPage * pPropertyPage = NULL;
     CMFCOutlookBarPaneButton * pOutlookButton = NULL;
     if ( PrepareContext( props.m_pButton, pOutlookButton, pPropertyPage ) ) {
-        // Now that we know everything related to the tooltip needed, we are obtaining the tooltip text.
+        
+        // Storing tooltip class instance in parameters.
         friendly_CMFCPropertySheet * pPropertySheet = static_cast < friendly_CMFCPropertySheet * > ( m_pPropertySheet );
         CToolTipCtrl * pToolTip = ( static_cast < friendly_CMFCOutlookBarPaneList * > ( &pPropertySheet->m_wndPane1 ) )->m_pToolTip;
         props.m_pToolTip = dynamic_cast < CMFCToolTipCtrl * > ( pToolTip );
+        
+        // Now we know everything related to this tooltip.
+        // Updating tooltip properties.
         return FillToolTipProperties( pOutlookButton, pPropertyPage, props );
     }
     return FALSE;
--- ToolTipDataProvider.h
+++ ToolTipDataProvider.h
@@ -2,11 +2,12 @@
 #include <afxtempl.h>
 
 /*
-* Classes to translate tooltip text requests received at the main frame to the corresponding elements (buttons) owners.
+* Classes to customize tooltip appearance when such customization is not explicitly supported by MFC core.
 */
 
 /*
 
+1. Tooltip main text.
 The problem is that the custom tooltip text messages are sent not to the controls the mouse is howering over,
 but to the top-level main frame of the application.
 To customize tooltip texts at the related control level, we do the following.
@@ -14,10 +15,28 @@ We are hooking the "get tooltip text" message at the main frame level.
 There is a function for this already, CMDIFrameWndEx::GetToolbarButtonToolTipText.
 Then we route the message to the related control (provided the control is subcribed for this message processing).
 
+2. Tooltip description.
+The description (additional text at the toolbar) is obtained in a similar way, via main frame of the application.
+We are hooking the "get tooltip description text" message at the main frame level.
+The hooked function is CFrameWnd::GetMessageString, it is designed to return description for toolbar buttons,
+and instead of button/component pointer it receives command ID.
+Buttons of CMFCOutlookBarPane have no command IDs internally, so we are generating command IDs for them, as
+the part of CMDIFrameWndEx::GetToolbarButtonToolTipText processing activity.
+
+3. Tooltip icon.
+MFC draws icons in the tooltip only when the tooltip is bound to the CMFCRibbonButton or when the undelying toolbar
+have the CImageList associated. CMFCOutlookBarPane have no CMFCRibbonButtons and have no image list in an invisible toolbar.
+To show the icon in the tooltip we create an invisible CMFCRibbonButton containing the icon needed.
+
 Usage:
 
 - Instantiate CToolTipDataProviderList in the main frame context.
-- Override CMDIFrameWndEx::GetToolbarButtonToolTipText at the main frame class, translate the call to the CToolTipDataProviderList::OnGetToolTipText.
+
+- Override CMDIFrameWndEx::GetToolbarButtonToolTipText at the main frame class,
+  translate the call to the CToolTipDataProviderList::OnGetToolTipText.
+
+- Override CFrameWnd::GetMessageString at the main frame class,
+  translate the call to the CToolTipDataProviderList::GetMessageString.
 
 	class MainFrame : public CMDIFrameWndEx {
 	...
@@ -27,9 +46,16 @@ Usage:
 			return m_tooltipProvider.OnGetToolTipText( pButton, strTTText );
 		}
 	...
+		virtual void GetMessageString( UINT nID, CString & rMessage ) const {
+			m_tooltipProvider.OnGetMessageString( nID, rMessage );
+		}
+	...
 
-- Instantiate (or inherit) CMFCPropertySheetToolTipDataProvider in the property sheet, passing CToolTipDataProviderList instance to its constructor.
-- Override CMFCPropertySheetToolTipDataProvider::GetToolTipText, update text of the CString& passed, return TRUE if the text is updated.
+- Instantiate (or inherit) CMFCPropertySheetToolTipDataProvider in the property sheet,
+  passing CToolTipDataProviderList instance to its constructor.
+
+- Override CMFCPropertySheetToolTipDataProvider::FillToolTipProperties, update CToolTipDataProviderProperties& passed,
+  (tooltip texts, icon handle), return TRUE if properties should be applied.
 
 	class ContextPropSheet : public CMFCPropertySheet, public CMFCPropertySheetToolTipDataProvider {
 	...
@@ -37,43 +63,62 @@ Usage:
 			: CMFCPropertySheet( L"Context", this )
 			, CMFCPropertySheetToolTipDataProvider( list, this ) {}
 	...
-		virtual BOOL GetToolTipText( CMFCOutlookBarPaneButton * pButton, CMFCPropertyPage * pPropertyPage, CString & strTTText ) {
-			strTTText = L"Decorated button text: ==> ";
-			strTTText += pButton->m_strText;
-			strTTText += L" <==";
+		virtual BOOL FillToolTipProperties( CMFCOutlookBarPaneButton * pButton, CMFCPropertyPage * pPropertyPage, CToolTipDataProviderProperties & props ) {
+			props.m_strText = L"Decorated button text: ==> ";
+			props.m_strText += pButton->m_strText;
+			props.m_strText += L" <==";
 			return TRUE;
 		}
 	...
 
-- If needed, implement another logic of tooltip processing using CToolTipDataProvider with OnGetToolTipText override.
+- If needed, implement another logic of tooltip processing using CToolTipDataProvider with FillToolTipProperties override.
 
 */
 
 
+// forward declarations
 class CToolTipDataProviderList;
-
 class CCustomMFCRibbonButton;
 
+/*
+* Tooltip parameters container.
+*/
 class CToolTipDataProviderProperties {
-private:
-	// declared but not defined
-	CToolTipDataProviderProperties( const CToolTipDataProviderProperties & );
-	CToolTipDataProviderProperties & operator=( const CToolTipDataProviderProperties & );
 public:
 	CToolTipDataProviderProperties( CMFCToolBarButton * pButton );
 	~CToolTipDataProviderProperties();
 
+	// text
 	CString m_strText;
+
+	// description
 	CString m_strDescription;
+
+	// icon
 	HICON m_hIcon;
+
+	// MFC tooltip properties
 	CMFCToolTipInfo m_props;
-	CMFCToolBarButton * m_pButton;
+	
+	// fields for the internal usage
+
+	// tooltip control to apply MFC properties to (can't be deduced from callbacks)
 	CMFCToolTipCtrl * m_pToolTip;
+	
+	// pointer to the button (to map command ID to)
+	CMFCToolBarButton * m_pButton;
+	
+	// CMFCRibbonButton owning pointer
 	CCustomMFCRibbonButton * m_pRibbonButton;
+
+private:
+	// declared but not defined
+	CToolTipDataProviderProperties( const CToolTipDataProviderProperties & );
+	CToolTipDataProviderProperties & operator=( const CToolTipDataProviderProperties & );
 };
 
 /*
-* Basic processor of tooltip text requests.
+* Basic processor of tooltip data requests.
 */
 class CToolTipDataProvider {
 
@@ -82,6 +127,8 @@ protected:
 	// Parent list.
 	CToolTipDataProviderList * m_list;
 
+	// Tooltip properties storage.
+	// Owns CToolTipDataProviderProperties instances.
 	CMap < UINT, UINT, CToolTipDataProviderProperties *, CToolTipDataProviderProperties * > m_toolTipDataMap;
 
 public:
@@ -92,21 +139,29 @@ public:
 	// Destructor. Calls Unsubscribe().
 	virtual ~CToolTipDataProvider();
 
-	// Subscribe this instance to "need tooltip text" requests routed via CToolTipDataProviderList.
+	// Subscribe this instance to "need tooltip data" requests routed via CToolTipDataProviderList.
 	// Separate method for cases when the list can't be provided in constructor.
 	void Subscribe( CToolTipDataProviderList * list );
 
-	// Unsubscribe this instance from "need tooltip text" requests.
+	// Unsubscribe this instance from "need tooltip data" requests.
 	// Separate method for cases when the list must be unbound before destructor.
 	void Unsubscribe();
 
 	// This method is called for all "need tooltip text" requests, including those not related to this instance.
-	// The method should filter requests and process only those, which are related to this instance.
-	// Should update strTTText and return TRUE if the request for the tooltip text is processed.
+	// The method filters requests and process only those, which are related to this instance.
+	// Should update strTTText and return TRUE if the request is processed.
+	// The default implementation calls FillToolTipProperties and does tooltip initialization.
 	virtual BOOL OnGetToolTipText( CMFCToolBarButton * pButton, CString & strTTText );
 
+	// This method is called for all "need tooltip description" requests, including those not related to this instance.
+	// The method filters requests and process only those, which are related to this instance.
+	// Should update rMessage and return TRUE if the request is processed.
+	// The default implementation uses text from m_toolTipDataMap pre-filled in OnGetToolTipText().
 	virtual BOOL OnGetMessageString( UINT nID, CString & rMessage );
 
+	// The method is called to fill tooltip properties.
+	// Should update props and return TRUE if properties should be applied.
+	// The default implementation does nothing.
 	virtual BOOL FillToolTipProperties( CToolTipDataProviderProperties & props );
 };
 
@@ -131,12 +186,14 @@ public:
 	// Translates the tooltip text request to all registered processors.
 	BOOL OnGetToolTipText( CMFCToolBarButton * pButton, CString & strTTText );
 
+	// Should be called from CFrameWnd::GetMessageString.
+	// Translates the tooltip description text request to all registered processors.
 	void OnGetMessageString( UINT nID, CString & rMessage ) const;
 };
 
 
 /*
-* CMFCPropertySheet-specific processor of tooltip text requests.
+* CMFCPropertySheet-specific processor of tooltip data requests.
 */
 class CMFCPropertySheetToolTipDataProvider : public CToolTipDataProvider {
 
@@ -145,28 +202,23 @@ protected:
 	// Parent sheet.
 	CMFCPropertySheet * m_pPropertySheet;
 
+	// Locates parent MFC structures of button provided that button is a child of this property sheet.
 	BOOL PrepareContext( CMFCToolBarButton * pToolBarButton, CMFCOutlookBarPaneButton *& pOutlookBarPaneButton, CMFCPropertyPage *& pPropertyPage );
 
-	// Contains CMFCPropertySheet-specific logic. Calls GetToolTipText if notification is related to m_pPropertySheet.
+	// Contains CMFCPropertySheet-specific logic.
+	// Fills props.m_pToolTip field with a pointer to the the corresponding object owned by m_pPropertySheet.
+	// Calls public overloaded FillToolTipProperties() variation if notification is related to m_pPropertySheet.
 	virtual BOOL FillToolTipProperties( CToolTipDataProviderProperties & props );
 
 public:
 
-	// Subscribes to "need tooltip text" requests routed via CToolTipDataProviderList.
-	// Saves CMFCPropertySheet ponter to filter "need tooltip text" requests.
+	// Subscribes to "need tooltip data" requests routed via CToolTipDataProviderList.
+	// Stores CMFCPropertySheet ponter to filter "need tooltip data" requests.
 	CMFCPropertySheetToolTipDataProvider( CToolTipDataProviderList * list, CMFCPropertySheet * propertySheet );
 
-	// This method is called for all "need tooltip text" requests related to the CMFCPropertySheet passed via constructor.
-	// Should update strTTText and return TRUE if the request for the tooltip text is processed.
-	// The related button and property page objects are deduced and are passed to the function.
+	// This method is called for all "need tooltip data" requests related to the CMFCPropertySheet passed via constructor.
+	// Should update props and return TRUE if the request for the tooltip text is processed.
 	// Note that HWND-related structures in pPropertyPage can be in non-initialized state if the corresponding page was not activated yet.
 	virtual BOOL FillToolTipProperties( CMFCOutlookBarPaneButton * pButton, CMFCPropertyPage * pPropertyPage, CToolTipDataProviderProperties & props );
 
-	/*
-	virtual BOOL GetToolTipDescription( CMFCOutlookBarPaneButton * pButton, CMFCPropertyPage * pPropertyPage, CString & rMessage );
-
-	virtual BOOL GetToolTipIcon( CMFCOutlookBarPaneButton * pButton, CMFCPropertyPage * pPropertyPage, HICON & hIcon );
-
-	virtual BOOL GetToolTipProperties( CMFCOutlookBarPaneButton * pButton, CMFCPropertyPage * pPropertyPage, CMFCToolTipInfo & props );
-	*/
 };
